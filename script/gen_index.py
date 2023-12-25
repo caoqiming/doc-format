3b6d1bf8a00cf9901019bff522aa9295a6b0fbcc
caoqiming.cpp <caoqiming.cpp@bytedance.com>
2023-12-25 21:16:51 +0800

    format

--- script/gen_index.py
+++ script/gen_index.py
@@ -14,19 +14,26 @@ layout: home
 ---
 """
 
+category_head = """---
+title: %s
+layout: home
+---
+"""
+
 
 class tree:
-    def __init__(self, name, depth):
-        self.name = name
+    def __init__(self, name, path, depth):
+        self.name = name  # 这里是路径里的文件名
         self.data = {}
         self.file = None
+        self.path = path
         self.depth = depth
 
 
 class docfile:
-    category_map = {"speech_scrips": "演讲稿"}
+    category_map = {"speech_scrips": "演讲稿", "data_base": "数据库", "command": "命令"}
 
-    def get_category(self, key):
+    def get_category(self, key):  # 将文件名转化为显示的名字
         if key in docfile.category_map:
             return docfile.category_map[key]
         return key
@@ -71,25 +78,38 @@ class docfile:
 
     def gen_index(self, file_list):
         r = gen_tree(file_list)
-        with open("./doc/index.md", "w") as f:
-            f.write(home_head)
+        # 为每个文件夹生成一个索引文件，以构建左边的导航栏
+        deep_first_gen_index_file(r)
+
+
+def deep_first_gen_index_file(p):  # 为所有文件夹生成一个索引文件，如果还没有的话
+    if not p.file:
+        # TODO 检查是否已经有该索引文件，避免覆盖
+        index_path = "%s/%s_category.md" % (p.path, p.name)
+        with open(index_path, "w") as f:
+            f.write(category_head % (docfile.get_category(None, p.name)))
             lines = []
-            deep_first(r, lines)
+            deep_first_gen_index_content(p, p.depth, lines)
             for line in lines:
                 f.write(line)
 
+    for k in p.data:
+        deep_first_gen_index_file(p.data[k])
+    return
 
-def deep_first(p, lines):
+
+def deep_first_gen_index_content(p, base_depth, lines):  # 生成给定节点下的索引文件的内容，储存在lines中
     if p.file:
         path = "./" + p.file.path[6:][:-3]  # remove ./doc and .md
         lines.append("- [%s](%s)\n" % (p.file.title, path))
     else:
         lines.append(
-            "\n%s %s\n\n" % ("#" * p.depth, docfile.get_category(None, p.name))
+            "\n%s %s\n\n"
+            % ("#" * (p.depth - base_depth + 1), docfile.get_category(None, p.name))
         )
 
     for k in p.data:
-        deep_first(p.data[k], lines)
+        deep_first_gen_index_content(p.data[k], base_depth, lines)
 
 
 def findAllFile(base):
@@ -103,13 +123,13 @@ def findAllFile(base):
 
 
 def gen_tree(file_list) -> tree:
-    root = tree("doc", 1)
-    for df in file_list:
+    root = tree("doc", "./doc", 1)
+    for df in file_list:  # df 是文件，不包括文件夹
         p = root
         path_list = df.path.split("/")[2:]  # 取./doc/之后的部分
-        for name in path_list:
+        for name in path_list:  # p 从root 走到 df对应的文件，把中途缺失的节点都创建出来
             if not name in p.data:
-                p.data[name] = tree(name, p.depth + 1)
+                p.data[name] = tree(name, p.path + "/" + name, p.depth + 1)
             p = p.data[name]
         p.file = df
     return root
